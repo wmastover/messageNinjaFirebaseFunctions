5692b46021647f8ab109417b613251e064cb12ae
System Administrator <root@Williams-MacBook-Pro.local>
2023-05-21 23:46:07 +0100

    token creation working

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.createToken = functions.https.onCall(async (data, context) => {
+  const uid = data.uid;
+  
+  console.log("UID received: ", uid);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+  try {
+    const token = await admin.auth().createCustomToken(uid);
+    console.log("Token generated: ", token);
+    return token;
+  } catch (error) {
+    console.error("Error creating custom token: ", error);
+    throw new functions.https.HttpsError('internal', 'Failed to create custom token', error);
+  }
+});
